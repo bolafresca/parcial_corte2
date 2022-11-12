05c930ea1bf638a51fe6dd5b90481a95ea39106d
bolafresca <andresfjaimes13@protonmail.com>
2022-11-12 16:16:15 -0500

    tercer commit

--- registro.php
+++ registro.php
@@ -10,5 +10,7 @@ $conectar=conec();
 $sql="INSERT INTO datos (nombre, documento) VALUES ('$nombre', '$documento')";  
 
 ?>
+
+
 <!-- INSERT INTO datos (nombre, documento, fechaNacimiiento, departamento, ciudad, sexo, estadoCivil, tieneTrabajo, nombreEmpresa, Sueldo, hijos,estrato) 
 VALUES ('jorge', '546879', '2022-11-16', 'HUILA', 'LA PLATA', 'Masculino', 'Soltero', 'No', NULL, '36555', '2', '6'); -->
